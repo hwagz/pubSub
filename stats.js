d5d702c1edae0f31ddb6020c9b70a33c8b115af9
hwagz <hunt.wagner@gmail.com>
2016-04-08 22:09:50 -0400

    Project started, initial files added, some progress made

---
+++ stats.js
@@ -0,0 +1,20 @@
+var stats = (function(){
+  var people =0;
+  //cache DOM
+  var $stats = $('#statsModule');
+  var template = $('#stats-template').html();
+
+  _render();
+
+  function _render(){
+    $stats.html(Mustache.render(template,{people: people}))
+  }
+
+  function setPeople(newPeople){
+    people = newPeople;
+    _render();
+  }
+  return{
+    setPeople: setPeople
+  };
+})();
